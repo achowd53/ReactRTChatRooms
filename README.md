4b45cbe2c6288f04a209971b2ebc49edb7244bda
aribchowdhury <aribchowdhury03@gmail.com>
2024-01-13 22:33:35 -0500

    Socket-io client setup for chatroom

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ReactRTChatRooms
-Real-Time Chatrooms made using Node.JS, React (for client), PostgreSQL (for message cache), C++ (for socket server)
+Real-Time Chatrooms made using Node.JS, React (for client), PostgreSQL (for message cache), C++ (for socket server, up to 64 chatrooms possible)
--- my-app/src/component/ChatRoom.js
+++ my-app/src/component/ChatRoom.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { io } from 'socket.io-client';
 
 import "./ChatRoom.css";
 
@@ -9,11 +10,43 @@ export default class ChatRoom extends React.Component {
         this.state = { user: queryParameters.get("user"), 
                        serv: queryParameters.get("serv"), 
                        hist: [],
-                       msg: '' };
+                       msg: '',
+                       socket: io() };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.generateHistory = this.generateHistory.bind(this);
+        this.setupClient();
     }
+
+    setupClient() {
+        // Initial handshake with login server 28000 to confirm identity of client
+        var serverPort = 28000;
+        var socket = io('http://localhost:28000');
+        socket.on('connection', function() {
+            console.log('Client handshake with login server');
+            socket.emit('connection', `${this.state.user} ${this.state.serv}`);
+        });
+
+        // Server returns port number of correct server to call (28001-28065)
+        socket.on('data', (data) => {
+            serverPort = data;
+            socket.close();
+        });
+
+        // Setup socket for chat communication
+        this.setState({ socket: io('http://localhost:'+serverPort) });
+        this.state.socket.on('data', (data) => {
+            console.log('Received message');
+            this.state.hist.push(data.split(' '));
+        });
+        this.state.socket.on('error', (err) => {
+            console.log(err);
+        });
+        this.state.socket.on('end', function() {
+            console.log('Client disconnected');
+        });
+
+    };
 
     generateHistory() {
         return (
@@ -37,9 +70,10 @@ export default class ChatRoom extends React.Component {
 
     handleSubmit(event) {
         event.preventDefault();
-        if (this.state.msg != '') {
+        if (this.state.msg !== '') {
             var datetime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-            this.state.hist.push([datetime, this.state.user, this.state.msg]);
+            console.log(`Send message ${[datetime, this.state.user, this.state.msg].join(' ')} to server`);
+            this.state.socket.emit('data', [datetime, this.state.user, this.state.msg].join(' '));
             this.setState({ msg: '' });
         }
     };
