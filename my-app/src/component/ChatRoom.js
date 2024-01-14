e7182ada0e2ccb3d492ee0415008e3d2d4e8d867
aribchowdhury <aribchowdhury03@gmail.com>
2024-01-13 20:05:56 -0500

    Messages can be sent and loaded now, only by 1 person though

--- my-app/src/component/ChatRoom.js
+++ my-app/src/component/ChatRoom.js
@@ -5,37 +5,55 @@ import "./ChatRoom.css";
 export default class ChatRoom extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {user: '', serv: ''};
+        const queryParameters = new URLSearchParams(window.location.search)
+        this.state = { user: queryParameters.get("user"), 
+                       serv: queryParameters.get("serv"), 
+                       hist: [],
+                       msg: '' };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.generateHistory = this.generateHistory.bind(this);
     }
 
-    handleChange(event, state) {
-        if (state === "user") {
-            this.setState({ user : event.target.value });
-        } else {
-            this.setState({ serv : event.target.value });
-        }
+    generateHistory() {
+        return (
+            <table>
+                <tbody>
+                    {this.state.hist.map((row) => (
+                        <tr>
+                            {row.map((val, colId) => (
+                                <td id={colId}>{val}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        );
+    };
+    
+    handleChange(event) {
+        this.setState({ msg : event.target.value });
     };
 
     handleSubmit(event) {
         event.preventDefault();
-        if (this.state.user !== '' && this.state.serv !== '') {
-            return (
-                <div className="component-app">
-                    <ChatRoom/>
-                </div>
-            )
+        if (this.state.msg != '') {
+            var datetime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+            this.state.hist.push([datetime, this.state.user, this.state.msg]);
+            this.setState({ msg: '' });
         }
     };
 
     render() {
         return (
-            <div className="component-login-panel">
-                <h2> Login to Chatroom </h2>
+            <div className="component-chatroom-panel">
+                <h2> Logged in as user {this.state.user} in server {this.state.serv} </h2>
+                <div className="chatroom-history">
+                    {this.generateHistory()}
+                </div>
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" onChange={(e) => this.handleChange(e,"user")} value={this.state.user} placeholder="username" />
-                    <button type="submit">Login</button>
+                    <input type="text" onChange={this.handleChange} value={this.state.msg} placeholder="message" />
+                    <button type="submit">Send</button>
                 </form>
             </div>
         );
